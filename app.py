727882aec930597856b7dc486d0fabc80e1d85aa
Chiemezie Jacob <chiemeziejacob@Chiemezies-MacBook-Pro.local>
2026-01-30 01:45:06 +0100

    Implemented CORS for frontend

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ from config import Config
 from db import close_conn
 from auth_routes import auth_bp
 from sequence_routes import seq_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     JWTManager(app)
+
+    CORS(
+        app,
+        origins=["http://localhost:3000", "https://helixpace.vercel.app/"],
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allow_headers=["Content-Type", "Authorization"],
+    )
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(seq_bp)
